57dc58865e9616d6914786e6736f654c3e74e30a
juliomchado <juliocarlos00@hotmail.com>
2021-11-23 21:20:18 +0000

    add/ countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,76 @@ import type { NextPage } from 'next'
 
 import styles from './styles.module.scss';
 
+import { formatDuration, intervalToDuration } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useEffect, useState } from 'react';
+
+
 const Home: NextPage = () => {
+
+  const [timeToLaunch, setTimeToLaunch] = useState<string | null | undefined>('');
+
+  function calculateAndFormatTimeToLaunch() {
+
+    const startDate = new Date();
+    const endDate = new Date(2021, 11, 25, 12);
+
+
+    const duration = intervalToDuration(
+      {
+        start: startDate,
+        end: endDate
+      }
+    );
+
+    if (duration.days === 0
+      && duration.hours === 0
+      && duration.seconds === 0
+    ) {
+      setTimeToLaunch(null);
+    }
+    else {
+      const formattedDuration = formatDuration(duration, {
+        delimiter: ', ',
+        format: ['days', 'minutes', 'seconds'],
+        locale: ptBR
+      });
+
+
+      return formattedDuration
+    }
+
+
+
+  }
+
+  useEffect(() => {
+    const formattedValue = calculateAndFormatTimeToLaunch();
+
+
+    setTimeToLaunch(formattedValue);
+
+
+  }, []);
+
+  setInterval(() => {
+    if (timeToLaunch) {
+      const formattedValue = calculateAndFormatTimeToLaunch();
+
+
+      setTimeToLaunch(formattedValue);
+    }
+
+  }, 1000)
+
+
+
+  // console.log('distance normal' + distanceInIso);
+  // console.log('distance in Iso ' + distanceInIso)
+
+  // console.log('dateToLaunch: ' + dateToLaunch)
+  // console.log('date now: ' + dateNow)
+
   return (
     <main className={styles.container}>
       <section className={styles.titlesSection}>
@@ -10,7 +79,7 @@ const Home: NextPage = () => {
           Lançamento Oficial da Cachaça Córrego Novo
         </h1>
         <h2 className={styles.secondaryTitle}>
-          Dia 25 de Novembro
+          Dia 25 de Novembro às 12h
         </h2>
       </section>
 
@@ -21,7 +90,7 @@ const Home: NextPage = () => {
           Tempo para o lançamento
         </h5>
         <div className={styles.countdownContainer}>
-          <p>3 dias, 12 horas e 32 segundos</p>
+          <p>{timeToLaunch ? timeToLaunch : 'Produto Foi Lançado!!'}</p>
         </div>
       </section>
     </main>

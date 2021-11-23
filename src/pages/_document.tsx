05179c3c5c966a25f45dcbe55994d35349bc7ebc
juliomchado <juliocarlos00@hotmail.com>
2021-11-23 19:41:17 +0000

    add/ index page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unknown-property */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="images/logo.svg" type="image/svg" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html >
+        )
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import type { NextPage } from 'next'
 
+import styles from './styles.module.scss';
+
 const Home: NextPage = () => {
   return (
-    <h1>Hello world</h1>
+    <main className={styles.container}>
+      <section className={styles.titlesSection}>
+        <h1 className={styles.primaryTitle} >
+          Lançamento Oficial da Cachaça Córrego Novo
+        </h1>
+        <h2 className={styles.secondaryTitle}>
+          Dia 25 de Novembro
+        </h2>
+      </section>
+
+      <img src="images/logo.svg" alt="Logo Córrego Novo" />
+
+      <section className={styles.countdownSection}>
+        <h5 className={styles.countdownText}>
+          Tempo para o lançamento
+        </h5>
+        <div className={styles.countdownContainer}>
+          <p>3 dias, 12 horas e 32 segundos</p>
+        </div>
+      </section>
+    </main>
   )
 }
 
